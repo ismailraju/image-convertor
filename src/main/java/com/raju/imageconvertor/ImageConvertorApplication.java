42aeac85e0563eccef215c5895e88cb9489f4445
ismailraju <ismailhossainraju74@gmail.com>
2022-02-10 23:39:50 +0600

    inti

---
+++ src/main/java/com/raju/imageconvertor/ImageConvertorApplication.java
@@ -0,0 +1,13 @@
+package com.raju.imageconvertor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageConvertorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageConvertorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raju/imageconvertor/ImageConvertorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raju.imageconvertor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageConvertorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
